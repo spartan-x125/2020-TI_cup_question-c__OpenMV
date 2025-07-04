98c2db0917e0529fa07c79b4376248cca8e2acd5
spartan-x125 <Spartan_x@126.com>
2025-07-04 16:54:40 +0800

    修改了循迹部分

--- track.py
+++ track.py
@@ -1,16 +1,26 @@
 from image import threshold
 import sensor
-import time
+import time,image
 
-red=(11,22,33,44,55,66)#红色阈值
+minL=11
+maxL=22
+minA=33
+maxA=44
+minB=55
+maxB=66
+
+
+
+red=(minL,maxL,minA,maxA,minB,maxB)#红色阈值
 
 def find_red(img,roi):
     blobs=img.find_blobs([red],roi)
     img.draw_rectangle(blobs.x,blobs.y,blobs.w,blobs.h)
     return blobs
 
-def tracking(img):
-    roi1=[1,2,3,4]
-    roi2=[2,3,4,5]
-    roi3=[3,4,5,6]
-    img.find_blobs([red],roi1)
+def statistic_red(img,roi):
+    counter=0
+    blobs=img.find_blobs([red],roi)
+    if blobs: return 1
+    else: return 0
+
